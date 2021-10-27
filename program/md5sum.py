38c768dca21a3cbd57c481bf8050c209a9fe129a
LordRonz <aarontanhar2000@gmail.com>
2021-10-27 09:14:11 +0700

    feat: Update checksum

--- program/md5sum.py
+++ program/md5sum.py
@@ -5,7 +5,7 @@ file_name = 'filename.exe'
 original_md5 = '6941402abc4b2a76b9719d911420c592'  
 
 with open(file_name) as file_to_check:
-    data = file_to_check.read()    
+    data = file_to_check.read().encode()
     md5_returned = hashlib.md5(data).hexdigest()
 
 if original_md5 == md5_returned:
